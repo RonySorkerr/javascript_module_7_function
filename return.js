ea1cf87a40b2ae0a5047634bacc6411ff88bebbe
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-03 18:52:00 +0600

    return

--- return.js
+++ return.js
@@ -1,7 +1,15 @@
-function tenTimes(number){
-    const result = number * 10;
-    return result;
+// function tenTimes(number){
+//     const result = number * 10;
+//     return result;
+// }
+
+// const output = tenTimes(5);
+// console.log(result);
+
+function add(price1, price2){
+    const total = price1 + price2;
+    return total;
 }
 
-const output = tenTimes(5);
-console.log(result);
+const bill = add(54, 65);
+console.log('your total bill is :', bill);
