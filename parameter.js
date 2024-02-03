a7279641106664a64cec43bab7f8b0e15e6e199f
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-03 15:13:43 +0600

    parameter

---
+++ parameter.js
@@ -0,0 +1,27 @@
+function eatRice (){
+    console.log('wash your hands');
+    console.log('sit on the table');
+    console.log('take your plate');
+    console.log('eat as fast as you can');
+}
+
+// eatRice();
+
+function square(number){
+    console.log('your given number is : ' , number);
+    const borgo = number * number;
+    console.log('your square is : ' , borgo);
+}
+
+square(5);
+
+console.log('asdfasdfasdfasdfasdf=================');
+
+square(12);
+console.log('asfdadf')
+square(45);
+square(110);
+console.log('asfdadf')
+
+square(101);
+square(205);
