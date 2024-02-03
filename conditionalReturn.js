76aab997799548a0548833913649e39e6ee0b145
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-03 19:05:54 +0600

    dddd

---
+++ conditionalReturn.js
@@ -0,0 +1,23 @@
+function isEven(numer){
+    if(numer % 2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(isEven(5));
+console.log(isEven(85));
+console.log(isEven(86));
+
+
+function isOdd(number){
+    if(number%2===0){
+        return true;
+    }
+    return false;
+}
+
+console.log(isOdd(12))
+console.log(isOdd(55));
