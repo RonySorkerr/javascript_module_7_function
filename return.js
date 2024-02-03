b819dd689ce7be707490d2a5bcb8324b037c7c5e
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-03 18:58:07 +0600

    sum

--- return.js
+++ return.js
@@ -8,8 +8,20 @@
 
 function add(price1, price2){
     const total = price1 + price2;
-    return total;
+    return total; //the return statement returns the value......< I AM NOT GETTING IT>................
 }
 
 const bill = add(54, 65);
 console.log('your total bill is :', bill);
+
+function math(num1, num2){
+    const sum = num1 + num2;
+    const diff = num1 - num2;
+    const mult = sum * diff;
+    const result = mult / 2;
+    return result;
+
+}
+
+const result = math(55, 54);
+console.log(result);
