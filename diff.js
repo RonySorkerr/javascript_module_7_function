ef2b6c91681b67960dd6fb9c55952095233d7b81
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-03 18:33:36 +0600

    d

---
+++ diff.js
@@ -0,0 +1,8 @@
+ function diff(num1, num2){
+    const difference = num1 - num2;
+    console.log(num1, num2, 'difference is : ', difference);
+}
+
+const fahtersAge = 45;
+const myAge = 22;
+diff(fahtersAge, myAge);
