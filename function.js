99afefcdc7c6cf2d798ef6db39abb68d24a78645
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-03 15:02:19 +0600

    function adding

---
+++ function.js
@@ -0,0 +1,17 @@
+// function declaration
+const number = 45;
+function fanOff (){
+    console.log('fasdf');
+    console.log('dadada');
+    console.log('fan off ');
+}
+
+function myName(){
+    console.log('My name is : Md.Rony Sorker');
+    console.log("My father's name is : Md.Esa miah ");
+    console.log("My mother's name is : Jarna Akter Lovely ");
+}
+// calling the function
+
+fanOff();
+myName();
